439c5923e3d8031d5751705e8c076bded267becf
xiaodaxia <xiaolei@huored.com>
2022-08-16 19:37:35 +0800

    :bug:add holly.pac and holly_precise.pac

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import base64
+import urllib.parse
 import json
 import logging
-import urllib.error
-import urllib.parse
-import urllib.parse
-import urllib.request
+import urllib.request, urllib.error, urllib.parse
 from argparse import ArgumentParser
+import base64
 
 __all__ = ['main']
 
@@ -33,6 +31,9 @@ def parse_args():
     parser.add_argument('--user-rule', dest='user_rule',
                         help='user rule file, which will be appended to'
                              ' gfwlist')
+    parser.add_argument('--precise', dest='precise', action='store_true',
+                        help='use adblock plus algorithm instead of O(1)'
+                             ' lookup')
     return parser.parse_args()
 
 
@@ -71,6 +72,80 @@ def combine_lists(content, user_rule=None):
     if user_rule:
         gfwlist.extend(user_rule.splitlines(False))
     return gfwlist
+
+
+def parse_gfwlist(gfwlist):
+    domains = set()
+    for line in gfwlist:
+        line = u(line)
+        if line.find('.*') >= 0:
+            continue
+        elif line.find('*') >= 0:
+            line = line.replace('*', '/')
+        if line.startswith('||'):
+            line = line.lstrip('||')
+        elif line.startswith('|'):
+            line = line.lstrip('|')
+        elif line.startswith('.'):
+            line = line.lstrip('.')
+        if line.startswith('!'):
+            continue
+        elif line.startswith('['):
+            continue
+        elif line.startswith('@'):
+            # ignore white list
+            continue
+        add_domain_to_set(domains, line)
+    return domains
+
+
+def reduce_domains(domains):
+    # reduce 'www.google.com' to 'google.com'
+    # remove invalid domains
+    tld_content = u(open('resources/tld.txt').read())
+    tlds = set(tld_content.splitlines(False))
+    new_domains = set()
+    for domain in domains:
+        domain_parts = domain.split('.')
+        last_root_domain = None
+        for i in range(0, len(domain_parts)):
+            root_domain = '.'.join(domain_parts[len(domain_parts) - i - 1:])
+            if i == 0:
+                if not tlds.__contains__(root_domain):
+                    # root_domain is not a valid tld
+                    break
+            last_root_domain = root_domain
+            if tlds.__contains__(root_domain):
+                continue
+            else:
+                break
+        if last_root_domain is not None:
+            new_domains.add(last_root_domain)
+
+    uni_domains = set()
+    for domain in new_domains:
+        domain_parts = domain.split('.')
+        for i in range(0, len(domain_parts)-1):
+            root_domain = '.'.join(domain_parts[len(domain_parts) - i - 1:])
+            if domains.__contains__(root_domain):
+                break
+        else:
+            uni_domains.add(domain)
+    return uni_domains
+
+
+def generate_pac_fast(domains, proxy):
+    # render the pac file
+    proxy_content = u(open('resources/proxy.pac').read())
+    domains_dict = {}
+    for domain in domains:
+        domains_dict[domain] = 1
+    proxy_content = proxy_content.replace('__PROXY__', json.dumps(str(proxy)))
+    proxy_content = proxy_content.replace(
+        '__DOMAINS__',
+        json.dumps(domains_dict, indent=2, sort_keys=True)
+    )
+    return proxy_content
 
 
 def generate_pac_precise(rules, proxy):
@@ -116,8 +191,12 @@ def main():
     gfwlist = []
     for byte_gfw in byte_gfwlist:
         gfwlist.append(byte_gfw.decode('utf-8'))
-    pac_content = generate_pac_precise(gfwlist, args.proxy)
-
+    if args.precise:
+        pac_content = generate_pac_precise(gfwlist, args.proxy)
+    else:
+        domains = parse_gfwlist(gfwlist)
+        domains = reduce_domains(domains)
+        pac_content = generate_pac_fast(domains, args.proxy)
     with open(args.output, 'w') as f:
         f.write(pac_content)
 
